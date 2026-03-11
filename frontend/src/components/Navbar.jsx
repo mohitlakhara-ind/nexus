5a0cb3d3d5917eba11553c28b2c47b1c773246f3
Mohit Lakhara <mohitlakhara78@gmail.com>
2026-03-11 09:33:17 +0530

    fix: resolve useState ReferenceError and static asset 404s

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 import useThemeStore from '../store/themeStore';
